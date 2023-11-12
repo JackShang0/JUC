520ea40c5460eee3b48f0e1d670b22e548bacd08
shangqj <shangqj@si-tech.com.cn>
2023-11-12 15:25:46 +0800

    unsafe类

---
+++ src/main/java/com/juc/juc07_cas/UnSafeDemo.java
@@ -0,0 +1,19 @@
+package com.juc.juc07_cas;
+
+import sun.misc.Unsafe;
+
+/**
+ * @description:
+ * @author: shangqj
+ * @date: 2023/8/8
+ * @version: 1.0
+ */
+public class UnSafeDemo {
+    private static final Unsafe unsafe = Unsafe.getUnsafe();
+    private static long valueOffset;
+
+    public static void main(String[] args) {
+
+
+    }
+}
